e081f4fcd4a887629282e041ddac7871912b6d03
Mithul Manivannan <mithulmanimala@gmail.ccom>
2023-12-09 13:25:35 -0800

    Done

--- 2023/Day9-Part2.py
+++ 2023/Day9-Part2.py
@@ -1,11 +1,9 @@
 t = 0
 for _ in range(int(input())):
-    arr = list(map(int, input().split()))
-    t += arr[0]
-    dir = -1
+    arr = list(map(int, input().split()))[::-1]
+    t += arr[-1]
     arr = [arr[i + 1] - arr[i] for i in range(len(arr) - 1)]
     while any(i for i in arr):
-        t += arr[0] * dir
-        dir *= -1
+        t += arr[-1]
         arr = [arr[i + 1] - arr[i] for i in range(len(arr) - 1)]
 print(t)
